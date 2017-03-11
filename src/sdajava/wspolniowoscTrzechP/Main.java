a5858298ae330a5245b2502f296aa3f909e17c6b
andrzej667 <abiczyk@gmail.com>
2017-03-11 15:18:24 +0100

    add final file

---
+++ src/sdajava/wspolniowoscTrzechP/Main.java
@@ -0,0 +1,47 @@
+package sdajava.wspolniowoscTrzechP;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int aX,aY,bX,bY,cX,cY;
+
+
+    public void dane() {
+
+        Scanner wejscie = new Scanner(System.in);
+        System.out.print("Podaj wspolrzedne dla:" + "\naX:  ");
+        this.aX = wejscie.nextInt();
+        System.out.print("aY   ");
+        this.aY = wejscie.nextInt();
+        System.out.print("bX   ");
+        this.bX = wejscie.nextInt();
+        System.out.print("bY   ");
+        this.bY = wejscie.nextInt();
+        System.out.print("cX   ");
+        this.cX = wejscie.nextInt();
+        System.out.print("cY   ");
+        this.cY = wejscie.nextInt();
+    }
+
+    public void doMath() {
+
+        int det = this.aX * this.bY + this.bX * this.cY + this.cX * this.aY - this.cX*this.bY- this.aX * this.cX - this.bX * this.aY;
+
+        if (det > 0) {
+            System.out.print("Punkt c znajduje się po lewej stronie wektora AB");
+        } else if (det == 0) {
+            System.out.print("Punkty są wspoliniowe");
+        } else {
+            System.out.print("Punkt c znajduje się po prawej stronie wektora AB");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Main wywolanie = new Main();
+        wywolanie.dane();
+        wywolanie.doMath();
+
+    }
+}
